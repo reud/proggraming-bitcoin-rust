864b691ca61ebecb41b40731defdb7c1a2dde527
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2021-01-29 20:57:02 +0900

    add 練習問題3

--- src/bin/field_element.rs
+++ src/bin/field_element.rs
@@ -1,48 +1,52 @@
 use std::fmt::{Display, Formatter};
-use crate::FieldElementError::InitializeError;
 use std::fmt;
+use std::ops::Add;
+use std::process::Output;
 
+// Debugの自動実装
+#[derive(Debug)]
 struct FieldElement {
     num: u64,
     prime: u64
 }
 
+// P.5 練習問題1
 impl PartialEq for FieldElement {
     fn eq(&self, other: &Self) -> bool {
         return self.num == other.num && self.prime == other.prime;
     }
 }
 
-// Debugの自動実装
-#[derive(Debug)]
-enum FieldElementError {
-    InitializeError(u64,u64,String)
-}
+impl Add for FieldElement {
+    type Output = FieldElement;
 
-impl Display for FieldElementError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        // matchを利用する。
-        use self::FieldElementError::*;
-        match self {
-            InitializeError(n,p,s) => write!(f, "InitializeError: msg: {} num: {} prime: {}",s,n,p)
+    // 左側のprimeに依存させる。
+    fn add(self, rhs: FieldElement) -> FieldElement {
+        Self::Output{
+            prime: self.prime,
+            num: (self.num+rhs.num).rem_euclid(self.prime)
         }
     }
+
 }
 
-fn new_field_element(num: u64, prime: u64) -> Result<FieldElement,FieldElementError> {
-    if num >= prime || num < 0 {
-        return Err(InitializeError(num, prime, String::from("bad argument")));
+impl Display for FieldElement {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f,"{}",self.num)
     }
-    return Ok(FieldElement{
-        num,
+}
+
+fn new_field_element(num: i64, prime: u64) -> FieldElement {
+    FieldElement{
+        num: num.rem_euclid(prime as i64) as u64,
         prime
-    });
+    }
 }
 
 fn main() {
     {
-        let a = new_field_element(7,13).unwrap();
-        let b = new_field_element(6,13).unwrap();
+        let a = new_field_element(7,13);
+        let b = new_field_element(6,13);
 
         println!("{}",a == b);
         println!("{}",a != b);
@@ -54,9 +58,27 @@ fn main() {
 
     // P.9 練習問題2
     {
-        let a = new_field_element(44,57).unwrap();
-        let b = new_field_element(44,57).unwrap();
-        // TODO 加算の追加
-        println!("{}",a == b);
+        {
+            let a = new_field_element(44,57);
+            let b = new_field_element(33,57);
+            println!("{}",a + b);
+        }
+        {
+            let a = new_field_element(9,57);
+            let b = new_field_element(-29,57);
+            println!("{}", a + b);
+        }
+        {
+            let a = new_field_element(17,57);
+            let b = new_field_element(42,57);
+            let c = new_field_element(49,57);
+            println!("{}", a + b + c);
+        }
+        {
+            let a = new_field_element(52,57);
+            let b = new_field_element(-30,57);
+            let c = new_field_element(-38,57);
+            println!("{}", a + b + c);
+        }
     }
 }
